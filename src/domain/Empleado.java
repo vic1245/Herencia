7b30217e1e53c372ce542483f331ef70dcc28014
Victor <vic12448@hotmail.com>
2023-08-25 20:08:49 -0500

    Practica de curso de java

---
+++ src/domain/Empleado.java
@@ -0,0 +1,47 @@
+package domain;
+
+public class Empleado extends Persona {
+
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleado;
+
+    public Empleado() {
+        //super();
+        this.idEmpleado = ++Empleado.contadorEmpleado;
+    }
+
+    public Empleado(String nombre, double sueldo) {
+//        super(nombre);        
+        this();
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(this.idEmpleado);
+        sb.append(", sueldo=").append(this.sueldo);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
